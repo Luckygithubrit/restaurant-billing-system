956a1f32f9d108cd8a5da8929042ce10b317aaf7
Laveen pappu <luckysteven5757@gmail.com>
2026-02-25 21:27:36 +0530

    change

--- restaurant.java
+++ restaurant.java
@@ -10,7 +10,7 @@ class Dish {
     }
 }
 
-public class Main {
+public class restaurant {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number of dishes:");
